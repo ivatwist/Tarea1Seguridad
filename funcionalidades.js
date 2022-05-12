e463d6bbfae99a14a91248f44d3ab0acd0eb5540
ivatwist <39283123+ivatwist@users.noreply.github.com>
2022-05-12 15:49:33 -0600

    update .js, .htmls y .css a como lo tengo

--- funcionalidades.js
+++ funcionalidades.js
@@ -34,7 +34,7 @@ function agregarACarrito(helado){
     }else if(nombreHelado=='pistacho'){
         contadorPistacho++;
     }else if(nombreHelado=='vainilla'){
-       contadorVainilla;
+       contadorVainilla++;
     }else if(nombreHelado=='fresa'){
         contadorFresa++;
     }else if(nombreHelado=='arandano'){
@@ -57,4 +57,34 @@ alert("Ha iniciado sesión correctamente.");
 }
 
 
+}
+
+function mostrarCarrito(){
+
+    if(contadorCiruela>0){
+       //mostrar la foto del helado de ciruela con su respectiva cantidad e iconos de + y -
+       alert("Hay "+contadorCiruela+" helados de ciruela.");
+    }
+
+    if(contadorVainilla>0){
+         //mostrar la foto del helado de vainilla con su respectiva cantidad e iconos de + y -
+    }
+
+    if(contadorArandano>0){
+
+         //mostrar la foto del helado de arandano con su respectiva cantidad e iconos de + y -
+    }
+
+
+    if(contadorLimon>0){
+         //mostrar la foto del helado de limon con su respectiva cantidad e iconos de + y -
+    }
+
+    if(contadorFresa>0){
+         //mostrar la foto del helado de fresa con su respectiva cantidad e iconos de + y -
+    }
+
+    if(contadorPistacho>0){
+         //mostrar la foto del helado de pistacho con su respectiva cantidad e iconos de + y -
+    }
 }
